ea709ef26c840fcc0eea4b14d39f6e4c87ef7a7f
raae <raae@bgraphic.no>
2013-04-05 14:29:10 +0200

    Alle itunes links work

--- bGraphicCommonStuff/BGInfoViewController.m
+++ bGraphicCommonStuff/BGInfoViewController.m
@@ -10,6 +10,16 @@
 #import "TestFlight.h"
 #import "BGCommonGraphics.h"
 
+static NSString * const kBGAppStoreID = @"522152533";
+static NSString * const kBGtwitterHandler = @"bGraphic_apps";
+static NSString * const kBGSupportMail = @"support@bgraphic.no";
+
+static NSString * const kBGAppBaseUrl = @"http://itunes.apple.com/app/id%@";
+static NSString * const kBGAppReviewBaseUrl = @"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@";
+static NSString * const kBGDevAppStoreBaseUrl = @"itms-apps://ax.itunes.apple.com/artist/id%@";
+static NSString * const kBGTwitterHttpBaseUrl = @"http://twitter.com/%@";
+static NSString * const kBGTwitterAppBaserUrl = @"twitter:///user?screen_name=%@";
+
 @interface BGInfoViewController ()
 -(BOOL)openUrl:(NSString *)urlString;
 @end
@@ -61,7 +71,7 @@
 }
 
 - (IBAction)rateAppAction:(id)sender {
-    NSString *rateAppUrl = [NSString stringWithFormat:@"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@", [self.infoDict objectForKey:@"app_id"]];
+    NSString *rateAppUrl = [NSString stringWithFormat:kBGAppReviewBaseUrl, [self.infoDict objectForKey:@"app_id"]];
     
     [TestFlight passCheckpoint:@"RATE"];
     
@@ -69,38 +79,38 @@
 }
 
 - (IBAction)shareAction:(id)sender {
-    NSString *shareAppUrl = [NSString stringWithFormat:@"http://itunes.apple.com/app/id%@", [self.infoDict objectForKey:@"app_id"]];
+    NSString *shareAppUrl = [NSString stringWithFormat:kBGAppBaseUrl, [self.infoDict objectForKey:@"app_id"]];
     NSString *body = [NSString stringWithFormat:NSLocalizedString(@"share_mail_body", nil), shareAppUrl];
     
     [self sendMailTo:nil withTitle:NSLocalizedString(@"share_mail_title", nil) andBody:body];
 }
 
 - (IBAction)supportAction:(id)sender {
-    [self sendMailTo:[self.infoDict objectForKey:@"support_mail"] withTitle:NSLocalizedString(@"support_mail_title", nil) andBody:nil];
+    [self sendMailTo:kBGSupportMail withTitle:NSLocalizedString(@"support_mail_title", nil) andBody:nil];
 }
 
 - (IBAction)blogAction:(id)sender {
-    [TestFlight passCheckpoint:@"BLOG"];
+    [TestFlight passCheckpoint:@"WEB"];
     
-    [self openUrl:[self.infoDict objectForKey:@"blog_link"]];
+    [self openUrl:[self.infoDict objectForKey:@"web_link"]];
 }
 
 - (IBAction)moreAppsAction:(id)sender {
     [TestFlight passCheckpoint:@"MORE APPS"];
     
     NSString *moreAppsUrl
-    = [NSString stringWithFormat:@"itms-apps://itunes.com/apps/%@", [self.infoDict objectForKey:@"store_id"]];
+    = [NSString stringWithFormat:kBGDevAppStoreBaseUrl, kBGAppStoreID];
     
     [self openUrl:moreAppsUrl];
 }
 
 - (IBAction)twitterAction:(id)sender {
-    NSString *twitterUrl = [NSString stringWithFormat:@"twitter:///user?screen_name=%@", [self.infoDict objectForKey:@"twitter_user"]];
+    NSString *twitterUrl = [NSString stringWithFormat:kBGTwitterHttpBaseUrl, kBGtwitterHandler];
     
     [TestFlight passCheckpoint:@"TWITTER"];
     
     if(![self openUrl:twitterUrl]) {
-        twitterUrl = [NSString stringWithFormat:@"http://twitter.com/%@", [self.infoDict objectForKey:@"twitter_user"]];
+        twitterUrl = [NSString stringWithFormat:kBGTwitterAppBaserUrl, kBGtwitterHandler];
         [self openUrl:twitterUrl];
     }
 }
