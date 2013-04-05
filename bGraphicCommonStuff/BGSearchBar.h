dea9132ff34f467859b24cfc78b1a2a313a83fa7
raae <raae@bgraphic.no>
2013-04-05 14:35:55 +0200

    Added search bar design

---
+++ bGraphicCommonStuff/BGSearchBar.h
@@ -0,0 +1,16 @@
+//
+//  BGSearchBar.h
+//  bGraphicCommonStuff
+//
+//  Created by Benedicte Raae on 05.04.13.
+//  Copyright (c) 2013 bGraphic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGSearchBar : UISearchBar
+
+@property UIView *borderView;
+@property (nonatomic, strong) UIColor *borderColor;
+
+@end
